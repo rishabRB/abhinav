06e0d9da4ec204a5ca4e39ea055f20c26c2e5736
rishab bibhuty <58904133+rishabRB@users.noreply.github.com>
2023-05-22 21:55:11 +0530

    mid-last commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { PhotoIcon, UserCircleIcon,} from '@heroicons/react/24/solid'
+import { LockClosedIcon, PhotoIcon, UserCircleIcon,} from '@heroicons/react/24/solid'
 
 
 function Navbar() {
   return (
     <nav className="sticky top-0 bg-white w-full flex items-center justify-between p-4  shadow-sm">
     <div onClick={()=> {}} className="flex items-center space-x-2 px-2">
-      {/* <PhotoIcon className='h-8 w-8  text-indigo-500' /> */}
+      <LockClosedIcon className='h-6 w-6  text-indigo-500' />
     <div className="text-black uppercase font-bold tracking-[8px] font-Ubuntu text-xl">
         ImagEncrypt
     </div>
