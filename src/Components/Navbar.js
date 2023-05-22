bac89a9865ea83b8a6b622eb246db056a87df92c
rishab bibhuty <58904133+rishabRB@users.noreply.github.com>
2023-05-22 21:56:51 +0530

    mid-last commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <nav className="sticky top-0 bg-white w-full flex items-center justify-between p-4  shadow-sm">
     <div onClick={()=> {}} className="flex items-center space-x-2 px-2">
-      <LockClosedIcon className='h-6 w-6  text-indigo-500' />
+      <LockClosedIcon className='h-6 w-6  text-black mb-1' />
     <div className="text-black uppercase font-bold tracking-[8px] font-Ubuntu text-xl">
         ImagEncrypt
     </div>
